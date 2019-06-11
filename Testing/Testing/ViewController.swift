c95457c60ba67aaa5689f511178369aa7084faaf
Elbert Luta <elbertluta@Elberts-MacBook-Pro.local>
2019-06-11 11:55:07 +0800

    Add new project

---
+++ Testing/Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Elbert Luta on 11/06/2019.
+//  Copyright © 2019 Elbert Luta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
